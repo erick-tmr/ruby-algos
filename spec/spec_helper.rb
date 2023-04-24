dd61e13fa85fc26173223a522e4f3b1517c91d11
Erick Takeshi <erick.rezende@iugu.com>
2023-04-24 09:11:01 -0300

    add to path exercises folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -71,6 +71,10 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 
+  # Load exercises path
+  app_path = File.expand_path("../../exercises", __FILE__)
+  $LOAD_PATH.unshift app_path
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
