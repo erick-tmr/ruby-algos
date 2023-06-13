8815215bee23345a537b221469391ffc183e6c2c
Erick Takeshi <erick.tmr@outlook.com>
2023-06-13 19:41:50 -0300

    add api test

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 
 gem 'rspec', '~> 3.12'
 gem 'pry', '~> 0.14.2'
+gem 'openpix-ruby_sdk'
---
+++ test_api.rb
@@ -0,0 +1,31 @@
+require 'openpix/rubysdk'
+
+# Your AppID from https://app.woovi.com/home/applications/tab/list
+app_id = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+client = Openpix::RubySdk::Client.new(app_id)
+
+# Creating a Charge
+puts "Creating Charge"
+client.charges.init_body(
+  params: {
+    correlation_id: 'my-correlation-id',
+    value: 50000
+  }
+)
+response = client.charges.save
+puts "response.success? - #{response.success?}"
+puts "##############"
+puts "response.resource_response: "
+puts response.resource_response
+
+# Listing Charges
+# Default skip is 0 and limit is 100
+puts "Listing Charge"
+response = client.charges.fetch(skip: 0, limit: 100) # skip and limit are pagination params, https://developers.woovi.com/api#tag/charge/paths/~1api~1v1~1charge/get
+puts "##############"
+puts "response.pagination_meta: "
+puts response.pagination_meta
+puts "##############"
+puts "response.resource_response: "
+puts response.resource_response # API response for this resource, should be an array
